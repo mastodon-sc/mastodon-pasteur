834172595a0f2d6f56ec1409b1b1ee378fe8cd38
Jean-Yves TINEVEZ <jean-yves.tinevez@pasteur.fr>
2019-03-29 13:49:37 +0100

    GUI to run the CSV importer.

---
+++ src/main/java/org/mastodon/io/csv/ui/CSVImporterPanel.java
@@ -0,0 +1,310 @@
+package org.mastodon.io.csv.ui;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.text.NumberFormat;
+
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.JFormattedTextField;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JSeparator;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
+
+import org.mastodon.io.csv.CSVImporter;
+
+public class CSVImporterPanel extends JPanel
+{
+	private static final long serialVersionUID = 1L;
+
+	final JTextField textFieldFile;
+
+	final JButton btnBrowse;
+
+	final JButton btnImport;
+
+	final JComboBox< String > comboBoxXCol;
+
+	final JComboBox< String > comboBoxYCol;
+
+	final JComboBox< String > comboBoxZCol;
+
+	final JComboBox< String > comboBoxFrameCol;
+
+	final JComboBox< String > comboBoxQualityCol;
+
+	final JComboBox< String > comboBoxNameCol;
+
+	final JComboBox< String > comboBoxIDCol;
+
+	final JComboBox< String > comboBoxTrackCol;
+
+	final JCheckBox chckbxImportTracks;
+
+	final JLabel labelRadiusUnit;
+
+	final JFormattedTextField ftfRadius;
+
+	public CSVImporterPanel()
+	{
+		setBorder( new EmptyBorder( 5, 5, 5, 5 ) );
+		final Font bigFont = getFont().deriveFont( getFont().getSize2D() + 2f );
+
+		final BorderLayout borderLayout = new BorderLayout();
+		setLayout( borderLayout );
+
+		final JPanel panelControl = new JPanel();
+		add( panelControl, BorderLayout.SOUTH );
+		panelControl.setBorder( null );
+		final GridBagLayout layout = new GridBagLayout();
+		layout.rowHeights = new int[] { 5, 5, 5, 5, 5, 0, 5, 5, 5, 5, 5, 5, 5, 5, 0 };
+		layout.columnWeights = new double[] { 1.0, 1.0, 0.0, 1.0, 1.0 };
+		layout.columnWidths = new int[] { 79, 50, 30, 50, 30 };
+		panelControl.setLayout( layout );
+
+		final JLabel lblCsvFile = new JLabel( "CSV file:" );
+		final GridBagConstraints gbc_lblCsvFile = new GridBagConstraints();
+		gbc_lblCsvFile.anchor = GridBagConstraints.WEST;
+		gbc_lblCsvFile.insets = new Insets( 5, 5, 5, 5 );
+		gbc_lblCsvFile.gridx = 0;
+		gbc_lblCsvFile.gridy = 0;
+		panelControl.add( lblCsvFile, gbc_lblCsvFile );
+
+		textFieldFile = new JTextField();
+		final GridBagConstraints gbc_textField = new GridBagConstraints();
+		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField.insets = new Insets( 5, 5, 5, 0 );
+		gbc_textField.gridwidth = 5;
+		gbc_textField.gridx = 0;
+		gbc_textField.gridy = 1;
+		panelControl.add( textFieldFile, gbc_textField );
+		textFieldFile.setColumns( 5 );
+
+		btnBrowse = new JButton( "Browse" );
+		final GridBagConstraints gbc_btnBrowse = new GridBagConstraints();
+		gbc_btnBrowse.anchor = GridBagConstraints.EAST;
+		gbc_btnBrowse.gridwidth = 2;
+		gbc_btnBrowse.insets = new Insets( 5, 5, 5, 0 );
+		gbc_btnBrowse.gridx = 3;
+		gbc_btnBrowse.gridy = 2;
+		panelControl.add( btnBrowse, gbc_btnBrowse );
+
+		chckbxImportTracks = new JCheckBox( "Import tracks?" );
+		final GridBagConstraints gbc_chckbxImportTracks = new GridBagConstraints();
+		gbc_chckbxImportTracks.anchor = GridBagConstraints.EAST;
+		gbc_chckbxImportTracks.gridwidth = 5;
+		gbc_chckbxImportTracks.insets = new Insets( 5, 5, 5, 0 );
+		gbc_chckbxImportTracks.gridx = 0;
+		gbc_chckbxImportTracks.gridy = 3;
+		panelControl.add( chckbxImportTracks, gbc_chckbxImportTracks );
+		chckbxImportTracks.setVisible( false ); // TODO
+
+		final JSeparator separator = new JSeparator();
+		final GridBagConstraints gbc_separator = new GridBagConstraints();
+		gbc_separator.fill = GridBagConstraints.BOTH;
+		gbc_separator.gridwidth = 5;
+		gbc_separator.insets = new Insets( 5, 5, 5, 5 );
+		gbc_separator.gridx = 0;
+		gbc_separator.gridy = 4;
+		panelControl.add( separator, gbc_separator );
+
+		final JLabel lblRadius = new JLabel( "Radius:" );
+		final GridBagConstraints gbc_lblRadius = new GridBagConstraints();
+		gbc_lblRadius.anchor = GridBagConstraints.EAST;
+		gbc_lblRadius.insets = new Insets( 0, 0, 5, 5 );
+		gbc_lblRadius.gridx = 0;
+		gbc_lblRadius.gridy = 5;
+		panelControl.add( lblRadius, gbc_lblRadius );
+
+		ftfRadius = new JFormattedTextField( NumberFormat.getNumberInstance() );
+		ftfRadius.setHorizontalAlignment( SwingConstants.TRAILING );
+		final GridBagConstraints gbc_ftfRadius = new GridBagConstraints();
+		gbc_ftfRadius.gridwidth = 2;
+		gbc_ftfRadius.insets = new Insets( 5, 5, 5, 5 );
+		gbc_ftfRadius.fill = GridBagConstraints.HORIZONTAL;
+		gbc_ftfRadius.gridx = 1;
+		gbc_ftfRadius.gridy = 5;
+		panelControl.add( ftfRadius, gbc_ftfRadius );
+
+		labelRadiusUnit = new JLabel();
+		final GridBagConstraints gbc_labelRadiusUnitl = new GridBagConstraints();
+		gbc_labelRadiusUnitl.anchor = GridBagConstraints.WEST;
+		gbc_labelRadiusUnitl.insets = new Insets( 0, 0, 5, 5 );
+		gbc_labelRadiusUnitl.gridx = 3;
+		gbc_labelRadiusUnitl.gridy = 5;
+		panelControl.add( labelRadiusUnit, gbc_labelRadiusUnitl );
+
+		final JLabel lblXColumn = new JLabel( "X column:" );
+		final GridBagConstraints gbc_lblXColumn = new GridBagConstraints();
+		gbc_lblXColumn.anchor = GridBagConstraints.EAST;
+		gbc_lblXColumn.insets = new Insets( 5, 5, 5, 5 );
+		gbc_lblXColumn.gridx = 0;
+		gbc_lblXColumn.gridy = 6;
+		panelControl.add( lblXColumn, gbc_lblXColumn );
+
+		comboBoxXCol = new JComboBox<>();
+		final GridBagConstraints gbc_comboBoxX = new GridBagConstraints();
+		gbc_comboBoxX.gridwidth = 4;
+		gbc_comboBoxX.insets = new Insets( 5, 5, 5, 0 );
+		gbc_comboBoxX.fill = GridBagConstraints.HORIZONTAL;
+		gbc_comboBoxX.gridx = 1;
+		gbc_comboBoxX.gridy = 6;
+		panelControl.add( comboBoxXCol, gbc_comboBoxX );
+
+		final JLabel lblYColumn = new JLabel( "Y column:" );
+		final GridBagConstraints gbc_lblYColumn = new GridBagConstraints();
+		gbc_lblYColumn.anchor = GridBagConstraints.EAST;
+		gbc_lblYColumn.insets = new Insets( 5, 5, 5, 5 );
+		gbc_lblYColumn.gridx = 0;
+		gbc_lblYColumn.gridy = 7;
+		panelControl.add( lblYColumn, gbc_lblYColumn );
+
+		comboBoxYCol = new JComboBox<>();
+		final GridBagConstraints gbc_comboBoxY = new GridBagConstraints();
+		gbc_comboBoxY.gridwidth = 4;
+		gbc_comboBoxY.insets = new Insets( 5, 5, 5, 0 );
+		gbc_comboBoxY.fill = GridBagConstraints.HORIZONTAL;
+		gbc_comboBoxY.gridx = 1;
+		gbc_comboBoxY.gridy = 7;
+		panelControl.add( comboBoxYCol, gbc_comboBoxY );
+
+		final JLabel lblZColumn = new JLabel( "Z column:" );
+		final GridBagConstraints gbc_lblZColumn = new GridBagConstraints();
+		gbc_lblZColumn.anchor = GridBagConstraints.EAST;
+		gbc_lblZColumn.insets = new Insets( 5, 5, 5, 5 );
+		gbc_lblZColumn.gridx = 0;
+		gbc_lblZColumn.gridy = 8;
+		panelControl.add( lblZColumn, gbc_lblZColumn );
+
+		comboBoxZCol = new JComboBox<>();
+		final GridBagConstraints gbc_comboBoxZ = new GridBagConstraints();
+		gbc_comboBoxZ.gridwidth = 4;
+		gbc_comboBoxZ.insets = new Insets( 5, 5, 5, 0 );
+		gbc_comboBoxZ.fill = GridBagConstraints.HORIZONTAL;
+		gbc_comboBoxZ.gridx = 1;
+		gbc_comboBoxZ.gridy = 8;
+		panelControl.add( comboBoxZCol, gbc_comboBoxZ );
+
+		final JLabel lblFrameColumn = new JLabel( "Frame column:" );
+		final GridBagConstraints gbc_lblFrameColumn = new GridBagConstraints();
+		gbc_lblFrameColumn.anchor = GridBagConstraints.EAST;
+		gbc_lblFrameColumn.insets = new Insets( 5, 5, 5, 5 );
+		gbc_lblFrameColumn.gridx = 0;
+		gbc_lblFrameColumn.gridy = 9;
+		panelControl.add( lblFrameColumn, gbc_lblFrameColumn );
+
+		comboBoxFrameCol = new JComboBox<>();
+		final GridBagConstraints gbc_comboBoxFrame = new GridBagConstraints();
+		gbc_comboBoxFrame.gridwidth = 4;
+		gbc_comboBoxFrame.insets = new Insets( 5, 5, 5, 0 );
+		gbc_comboBoxFrame.fill = GridBagConstraints.HORIZONTAL;
+		gbc_comboBoxFrame.gridx = 1;
+		gbc_comboBoxFrame.gridy = 9;
+		panelControl.add( comboBoxFrameCol, gbc_comboBoxFrame );
+
+		final JLabel lblTrackColumn = new JLabel( "Track column:" );
+		final GridBagConstraints gbc_lblTrackColumn = new GridBagConstraints();
+		gbc_lblTrackColumn.anchor = GridBagConstraints.EAST;
+		gbc_lblTrackColumn.insets = new Insets( 5, 5, 5, 5 );
+		gbc_lblTrackColumn.gridx = 0;
+		gbc_lblTrackColumn.gridy = 10;
+		panelControl.add( lblTrackColumn, gbc_lblTrackColumn );
+		lblTrackColumn.setVisible( false ); // TODO
+
+		comboBoxTrackCol = new JComboBox<>();
+		final GridBagConstraints gbc_comboBoxTrackCol = new GridBagConstraints();
+		gbc_comboBoxTrackCol.gridwidth = 4;
+		gbc_comboBoxTrackCol.insets = new Insets( 5, 5, 5, 0 );
+		gbc_comboBoxTrackCol.fill = GridBagConstraints.HORIZONTAL;
+		gbc_comboBoxTrackCol.gridx = 1;
+		gbc_comboBoxTrackCol.gridy = 10;
+		panelControl.add( comboBoxTrackCol, gbc_comboBoxTrackCol );
+		chckbxImportTracks.addActionListener( ( e ) -> comboBoxTrackCol.setEnabled( chckbxImportTracks.isSelected() ) );
+		comboBoxTrackCol.setEnabled( chckbxImportTracks.isSelected() );
+		comboBoxTrackCol.setVisible( false ); // TODO
+
+		final JLabel lblQualityColumn = new JLabel( "Quality column:" );
+		final GridBagConstraints gbc_lblQualityColumn = new GridBagConstraints();
+		gbc_lblQualityColumn.anchor = GridBagConstraints.EAST;
+		gbc_lblQualityColumn.insets = new Insets( 5, 5, 5, 5 );
+		gbc_lblQualityColumn.gridx = 0;
+		gbc_lblQualityColumn.gridy = 11;
+		panelControl.add( lblQualityColumn, gbc_lblQualityColumn );
+
+		comboBoxQualityCol = new JComboBox<>();
+		final GridBagConstraints gbc_comboBoxQuality = new GridBagConstraints();
+		gbc_comboBoxQuality.gridwidth = 4;
+		gbc_comboBoxQuality.insets = new Insets( 5, 5, 5, 0 );
+		gbc_comboBoxQuality.fill = GridBagConstraints.HORIZONTAL;
+		gbc_comboBoxQuality.gridx = 1;
+		gbc_comboBoxQuality.gridy = 11;
+		panelControl.add( comboBoxQualityCol, gbc_comboBoxQuality );
+
+		final JLabel lblNameColumn = new JLabel( "Name column:" );
+		final GridBagConstraints gbc_lblNameColumn = new GridBagConstraints();
+		gbc_lblNameColumn.anchor = GridBagConstraints.EAST;
+		gbc_lblNameColumn.insets = new Insets( 5, 5, 5, 5 );
+		gbc_lblNameColumn.gridx = 0;
+		gbc_lblNameColumn.gridy = 12;
+		panelControl.add( lblNameColumn, gbc_lblNameColumn );
+
+		comboBoxNameCol = new JComboBox<>();
+		final GridBagConstraints gbc_comboBoxName = new GridBagConstraints();
+		gbc_comboBoxName.gridwidth = 4;
+		gbc_comboBoxName.insets = new Insets( 5, 5, 5, 0 );
+		gbc_comboBoxName.fill = GridBagConstraints.HORIZONTAL;
+		gbc_comboBoxName.gridx = 1;
+		gbc_comboBoxName.gridy = 12;
+		panelControl.add( comboBoxNameCol, gbc_comboBoxName );
+
+		final JLabel lblIdColumn = new JLabel( "ID column:" );
+		final GridBagConstraints gbc_lblIdColumn = new GridBagConstraints();
+		gbc_lblIdColumn.anchor = GridBagConstraints.EAST;
+		gbc_lblIdColumn.insets = new Insets( 5, 5, 5, 5 );
+		gbc_lblIdColumn.gridx = 0;
+		gbc_lblIdColumn.gridy = 13;
+		panelControl.add( lblIdColumn, gbc_lblIdColumn );
+
+		comboBoxIDCol = new JComboBox<>();
+		final GridBagConstraints gbc_comboBoxID = new GridBagConstraints();
+		gbc_comboBoxID.insets = new Insets( 5, 5, 5, 0 );
+		gbc_comboBoxID.gridwidth = 4;
+		gbc_comboBoxID.fill = GridBagConstraints.HORIZONTAL;
+		gbc_comboBoxID.gridx = 1;
+		gbc_comboBoxID.gridy = 13;
+		panelControl.add( comboBoxIDCol, gbc_comboBoxID );
+
+		final JPanel panelButtonExport = new JPanel();
+		final GridBagConstraints gbc_panelButtonExport = new GridBagConstraints();
+		gbc_panelButtonExport.anchor = GridBagConstraints.EAST;
+		gbc_panelButtonExport.gridwidth = 5;
+		gbc_panelButtonExport.gridx = 0;
+		gbc_panelButtonExport.gridy = 14;
+		panelControl.add( panelButtonExport, gbc_panelButtonExport );
+		final FlowLayout flowLayout = ( FlowLayout ) panelButtonExport.getLayout();
+		flowLayout.setAlignment( FlowLayout.RIGHT );
+
+		btnImport = new JButton( "Import" );
+		panelButtonExport.add( btnImport );
+
+		final JPanel panelTitle = new JPanel();
+		add( panelTitle, BorderLayout.NORTH );
+		panelTitle.setLayout( new BorderLayout( 0, 0 ) );
+
+		final JLabel lblCsvImporter = new JLabel( "Mastodon CSV importer", JLabel.CENTER );
+		lblCsvImporter.setFont( bigFont );
+		lblCsvImporter.setHorizontalAlignment( SwingConstants.CENTER );
+		panelTitle.add( lblCsvImporter, BorderLayout.NORTH );
+
+		final JLabel lblVersion = new JLabel( "v" + CSVImporter.PLUGIN_VERSION, JLabel.CENTER  );
+		panelTitle.add( lblVersion, BorderLayout.SOUTH );
+	}
+}
---
+++ src/main/java/org/mastodon/io/csv/ui/CSVImporterUIController.java
@@ -0,0 +1,319 @@
+package org.mastodon.io.csv.ui;
+
+import java.awt.FileDialog;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.Map;
+
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.mastodon.io.csv.CSVImporter;
+import org.mastodon.revised.model.mamut.Model;
+import org.scijava.log.Logger;
+import org.scijava.log.StderrLogService;
+
+public class CSVImporterUIController
+{
+	private static final String NONE_COLUMN = "Don't use";
+
+	private static final FileDialog dialog = new FileDialog( new JFrame(), "Open a CSV file", FileDialog.LOAD );
+
+	private final CSVImporterPanel view;
+
+	private File file;
+
+	private Map< String, Integer > headerMap;
+
+	private final Logger logger;
+
+	private final Model model;
+
+	public CSVImporterUIController( final Model model )
+	{
+		this.model = model;
+		this.view = new CSVImporterPanel();
+		view.btnBrowse.addActionListener( ( e ) -> browse() );
+		view.textFieldFile.addActionListener( ( e ) -> setCSVFile( new File( view.textFieldFile.getText() ) ) );
+		view.btnImport.addActionListener( ( e ) -> export() );
+		logger = new StderrLogService();
+
+		/*
+		 * Tentative automatic radius.
+		 */
+		view.labelRadiusUnit.setText( model.getSpaceUnits() );
+		final double r = 2.5;
+		view.ftfRadius.setValue( Double.valueOf( r ) );
+	}
+
+	public CSVImporterPanel getView()
+	{
+		return view;
+	}
+
+	private void export()
+	{
+		view.btnImport.setEnabled( false );
+		new Thread( "Mastodon CSV importer thread" )
+		{
+			@Override
+			public void run()
+			{
+				try
+				{
+					final String filePath = view.textFieldFile.getText();
+					final double radius = ( ( Number ) view.ftfRadius.getValue() ).doubleValue();
+
+					final CSVImporter importer = CSVImporter
+							.create()
+							.model( model )
+							.csvFilePath( filePath )
+							.radius( radius )
+							.xColumnName( ( String ) view.comboBoxXCol.getSelectedItem() )
+							.yColumnName( ( String ) view.comboBoxYCol.getSelectedItem() )
+							.zColumnName( ( String ) view.comboBoxZCol.getSelectedItem() )
+							.labelColumnName( ( String ) view.comboBoxNameCol.getSelectedItem() )
+							.frameColumnName( ( String ) view.comboBoxFrameCol.getSelectedItem() )
+							.qualityColumnName( ( String ) view.comboBoxQualityCol.getSelectedItem() )
+							.idColumnName( ( String ) view.comboBoxIDCol.getSelectedItem() )
+							.get();
+
+					if ( !importer.checkInput() || !importer.process() )
+					{
+						error( "Error importing CSV file:\n" + importer.getErrorMessage() );
+						return;
+					}
+					log( "CSV import successful.\n" );
+				}
+				finally
+				{
+					view.btnImport.setEnabled( true );
+				}
+			}
+		}.start();
+	}
+
+	private void browse()
+	{
+		final File file = askForCSVfile();
+		if ( null == file )
+			return;
+
+		setCSVFile( file );
+	}
+
+	private File askForCSVfile()
+	{
+		if ( null == file )
+			this.file = new File( System.getProperty( "user.home" ) );
+
+		dialog.setDirectory( file.getAbsolutePath() );
+		dialog.setFile( file.getName() );
+		dialog.setVisible( true );
+		String selectedFile = dialog.getFile();
+		if ( null == selectedFile )
+		{ return null; }
+		if ( !selectedFile.endsWith( ".csv" ) )
+			selectedFile += ".csv";
+		file = new File( dialog.getDirectory(), selectedFile );
+		return file;
+	}
+
+	public void setCSVFile( final File file )
+	{
+		this.file = file;
+		view.textFieldFile.setText( file.getAbsolutePath() );
+		log( "Inspecting CSV file: " + file + '\n' );
+
+		view.btnImport.setEnabled( false );
+		if ( readHeaders() )
+			view.btnImport.setEnabled( true );
+	}
+
+	private boolean readHeaders()
+	{
+		final String filePath = view.textFieldFile.getText();
+
+		/*
+		 * Open and parse file.
+		 */
+
+		Reader in;
+		CSVParser records;
+		try
+		{
+			in = new FileReader( filePath );
+		}
+		catch ( final FileNotFoundException e )
+		{
+			error( "Could not find CSV file:\n" + e.getMessage() + '\n' );
+			e.printStackTrace();
+			clearComboBoxes();
+			return false;
+		}
+
+		try
+		{
+			final CSVFormat csvFormat = CSVFormat.EXCEL
+					.withHeader()
+					.withCommentMarker( '#' );
+			records = csvFormat.parse( in );
+		}
+		catch ( final Exception e )
+		{
+			e.printStackTrace();
+			error( "Could not browse CSV file:\n" + e.getMessage() + "\n" );
+			clearComboBoxes();
+			return false;
+		}
+
+		this.headerMap = records.getHeaderMap();
+
+		// Iterate in column orders.
+		final ArrayList< String > headers = new ArrayList<>( headerMap.keySet() );
+		headers.removeIf( ( e ) -> e.trim().isEmpty() );
+
+		if ( headers.isEmpty() )
+		{
+			error( "Could not read the header of the CSV file.\nIt does not seem present.\n" );
+			return false;
+		}
+
+		final String[] mandatory = headers.toArray( new String[] {} );
+		view.comboBoxXCol.setModel( new DefaultComboBoxModel<>( mandatory ) );
+		view.comboBoxYCol.setModel( new DefaultComboBoxModel<>( mandatory ) );
+		view.comboBoxZCol.setModel( new DefaultComboBoxModel<>( mandatory ) );
+		view.comboBoxFrameCol.setModel( new DefaultComboBoxModel<>( mandatory ) );
+		view.comboBoxTrackCol.setModel( new DefaultComboBoxModel<>( mandatory ) );
+
+		// Try to be clever and guess from header names.
+		int tcol = -1;
+		int xcol = -1;
+		int ycol = -1;
+		int zcol = -1;
+		int trackcol = -1;
+		for ( int i = 0; i < mandatory.length; i++ )
+		{
+			final String current = mandatory[ i ];
+
+			if ( current.toLowerCase().startsWith( "x" ) || current.toLowerCase().endsWith( "x" ) )
+			{
+				if ( xcol < 0 || ( current.length() < mandatory[ xcol ].length() ) )
+					xcol = i;
+			}
+
+			if ( current.toLowerCase().startsWith( "y" ) || current.toLowerCase().endsWith( "y" ) )
+			{
+				if ( ycol < 0 || ( current.length() < mandatory[ ycol ].length() ) )
+					ycol = i;
+			}
+
+			if ( current.toLowerCase().startsWith( "z" ) || current.toLowerCase().endsWith( "z" ) )
+			{
+				if ( zcol < 0 || ( current.length() < mandatory[ zcol ].length() ) )
+					zcol = i;
+			}
+
+			if ( current.toLowerCase().startsWith( "frame" )
+					|| current.toLowerCase().startsWith( "time" )
+					|| current.toLowerCase().startsWith( "t" ) )
+			{
+				if ( tcol < 0 || current.equals( "frame" ) )
+					tcol = i;
+			}
+
+			if ( current.toLowerCase().startsWith( "track" ) || current.toLowerCase().startsWith( "traj" ) )
+			{
+				if ( trackcol < 0 || current.equals( "track" ) )
+					trackcol = i;
+			}
+		}
+		if ( tcol < 0 )
+			tcol = 0 % ( mandatory.length - 1 );
+		if ( xcol < 0 )
+			xcol = 1 % ( mandatory.length - 1 );
+		if ( ycol < 0 )
+			ycol = 2 % ( mandatory.length - 1 );
+		if ( zcol < 0 )
+			zcol = 3 % ( mandatory.length - 1 );
+		if ( trackcol < 0 )
+			trackcol = 4 % ( mandatory.length - 1 );
+
+		view.comboBoxXCol.setSelectedIndex( xcol );
+		view.comboBoxYCol.setSelectedIndex( ycol );
+		view.comboBoxZCol.setSelectedIndex( zcol );
+		view.comboBoxFrameCol.setSelectedIndex( tcol );
+		view.comboBoxTrackCol.setSelectedIndex( trackcol );
+
+		// Add a NONE for non mandatory columns
+		headers.add( NONE_COLUMN );
+		final String[] nonMandatory = headers.toArray( new String[] {} );
+		view.comboBoxQualityCol.setModel( new DefaultComboBoxModel<>( nonMandatory ) );
+		view.comboBoxNameCol.setModel( new DefaultComboBoxModel<>( nonMandatory ) );
+		view.comboBoxIDCol.setModel( new DefaultComboBoxModel<>( nonMandatory ) );
+
+		int idcol = headers.indexOf( NONE_COLUMN );
+		int qualitycol = headers.indexOf( NONE_COLUMN );
+		int namecol = headers.indexOf( NONE_COLUMN );
+		for ( int i = 0; i < nonMandatory.length; i++ )
+		{
+			final String current = nonMandatory[ i ];
+
+			if ( current.toLowerCase().startsWith( "id" )
+					|| current.toLowerCase().startsWith( "index" ) )
+				idcol = i;
+
+			if ( current.toLowerCase().startsWith( "name" ) )
+				namecol = i;
+
+			if ( current.toLowerCase().startsWith( "q" ) )
+				qualitycol = i;
+		}
+
+		view.comboBoxIDCol.setSelectedIndex( idcol );
+		view.comboBoxQualityCol.setSelectedIndex( qualitycol );
+		view.comboBoxNameCol.setSelectedIndex( namecol );
+		try
+		{
+			in.close();
+		}
+		catch ( final IOException e )
+		{
+			error( "Problem closing the CSV file:\n" + e.getMessage() + '\n' );
+			e.printStackTrace();
+		}
+		return true;
+	}
+
+	private void clearComboBoxes()
+	{
+		final ArrayList< JComboBox< String > > comboBoxes = new ArrayList<>();
+		comboBoxes.add( view.comboBoxXCol );
+		comboBoxes.add( view.comboBoxYCol );
+		comboBoxes.add( view.comboBoxZCol );
+		comboBoxes.add( view.comboBoxFrameCol );
+		comboBoxes.add( view.comboBoxQualityCol );
+		comboBoxes.add( view.comboBoxNameCol );
+		comboBoxes.add( view.comboBoxIDCol );
+		comboBoxes.add( view.comboBoxTrackCol );
+		for ( final JComboBox< String > cb : comboBoxes )
+			cb.setModel( new DefaultComboBoxModel<>() );
+	}
+
+	private void error( final String string )
+	{
+		logger.error( string );
+	}
+
+	private void log( final String string )
+	{
+		logger.info( string );
+	}
+}
