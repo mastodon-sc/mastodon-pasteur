b648ba164cd02eb58d9e2e1f77e08cd360f7f22e
Jean-Yves Tinevez <tinevez@pasteur.fr>
2024-05-03 11:24:43 +0200

    Tweak test drive for NN stats.

--- src/test/java/org/mastodon/mamut/nearest/TestNearestNeighborStats.java
+++ src/test/java/org/mastodon/mamut/nearest/TestNearestNeighborStats.java
@@ -1,9 +1,9 @@
 package org.mastodon.mamut.nearest;
 
+import java.awt.event.WindowAdapter;
 import java.io.IOException;
 import java.util.Locale;
 
-import javax.swing.JFrame;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
@@ -22,14 +22,25 @@ public class TestNearestNeighborStats
 		Locale.setDefault( Locale.ROOT );
 		UIManager.setLookAndFeel( UIManager.getSystemLookAndFeelClassName() );
 
-		try (Context context = new Context())
+//		try 
+		final Context context = new Context();
+//		{
+//			final ProjectModel projectModel = ProjectLoader.open( "samples/drosophila_crop.mastodon", context );
+		final ProjectModel projectModel = ProjectLoader.open( "/Users/tinevez/Google Drive/Mastodon/Datasets/Local/Uzuki2D/Choana.mastodon", context );
+		final MainWindow mw = new MainWindow( projectModel );
+//		mw.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+		mw.setVisible( true );
+		mw.addWindowListener( new WindowAdapter()
 		{
-			final ProjectModel projectModel = ProjectLoader.open( "samples/drosophila_crop.mastodon", context );
-			final MainWindow mw = new MainWindow( projectModel );
-			mw.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-			mw.setVisible( true );
+			@Override
+			public void windowClosed( final java.awt.event.WindowEvent e )
+			{
+				context.close();
+				System.exit( 0 );
+			};
+		} );
 
-			projectModel.getPlugins().getPluginActions().getActionMap().get( NearestObjectStatPlugin.SHOW_NEAREST_NEIGHBORS_STATS_DIALOG_ACTION ).actionPerformed( null );
-		}
+		projectModel.getPlugins().getPluginActions().getActionMap().get( NearestObjectStatPlugin.SHOW_NEAREST_NEIGHBORS_STATS_DIALOG_ACTION ).actionPerformed( null );
+//		}
 	}
 }
